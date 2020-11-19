09fc9c70ecfebafbfb9a1c39fd398a12d47fde2d
macbre <maciej.brencz@gmail.com>
2020-11-19 19:18:21 +0100

    screenshot.js

---
+++ screenshot.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const puppeteer = require('puppeteer');
+
+const path = require('path');
+const fs = require('fs');
+
+const dir = path.dirname(fs.realpathSync(__filename));
+const url = 'file://' + dir + '/index.html';
+
+console.log(`Rendering <${url}> ...`);
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({width: 1200, height: 600});
+  await page.goto(url, {waitUntil: 'networkidle2'});
+  await page.screenshot({path: 'tweet.png'});
+
+  await browser.close();
+})();
+
+console.log('Done');
