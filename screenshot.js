8045d81027533f83d6c46497deff15f412e6a08a
macbre <maciej.brencz@gmail.com>
2021-07-29 13:19:29 +0100

    Múli + Retime scale factor

--- screenshot.js
+++ screenshot.js
@@ -12,7 +12,7 @@ console.log(`Rendering <${url}> ...`);
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({width: 1067, height: 600});
+  await page.setViewport({width: 1067, height: 600, deviceScaleFactor: 2});
   await page.goto(url, {waitUntil: 'networkidle2'});
   await new Promise(resolve => setTimeout(resolve, 5000));
 
