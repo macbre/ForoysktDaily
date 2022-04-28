98d54f91554e9a9bda64580738d14b827ae42728
macbre <maciej.brencz@gmail.com>
2022-04-28 22:06:25 +0100

    screenshot.js: use npmlog

--- screenshot.js
+++ screenshot.js
@@ -3,22 +3,24 @@ const puppeteer = require('puppeteer');
 
 const path = require('path');
 const fs = require('fs');
+const log = require('npmlog');
 
 const dir = path.dirname(fs.realpathSync(__filename));
 const url = 'file://' + dir + '/index.html';
 
-console.log(`Rendering <${url}> ...`);
+log.info('Rendering', `<${url}> ...`);
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({width: 1067, height: 600, deviceScaleFactor: 2});
-  await page.goto(url, {waitUntil: 'networkidle2'});
-  await new Promise(resolve => setTimeout(resolve, 5000));
+  await page.goto(url, {waitUntil: 'networkidle0'});
+  log.info('Page loaded');
+  // await new Promise(resolve => setTimeout(resolve, 5000));
 
+  log.info('Taking a screenshot');
   await page.screenshot({path: 'tweet.png'});
 
   await browser.close();
+  log.info('Done');
 })();
-
-console.log('Done');
