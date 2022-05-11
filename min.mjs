68111973198ec5f94450e0fa87cbdd7edd991efc
macbre <maciej.brencz@gmail.com>
2022-05-11 22:20:35 +0100

    Introduce imagemin and min.mjs

---
+++ min.mjs
@@ -0,0 +1,24 @@
+import fs from 'fs/promises';
+
+import imagemin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import log from 'npmlog';
+
+
+(async () => {
+    log.info('imagemin', 'Optimizing tweet.png ...');
+
+    await imagemin(['tweet.png'], {
+        destination: 'build/tweet.png',
+        plugins: [
+            imageminPngquant({
+                quality: [0.6, 0.8]
+            })
+        ]
+    });
+
+    await fs.rm('tweet.png');
+    await fs.rename('build/tweet.png', 'tweet.png');
+
+    log.info('imagemin', 'Done');
+})();
