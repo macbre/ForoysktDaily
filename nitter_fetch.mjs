93311df6c829e2bf9cf4956b66d1e38ff6f5659f
macbre <maciej.brencz@gmail.com>
2024-11-26 00:56:40 +0000

    Fetch all tweets

--- nitter_fetch.mjs
+++ nitter_fetch.mjs
@@ -6,7 +6,8 @@ import log from 'npmlog';
 import puppeteer, {Page} from 'puppeteer';
 
 const DOMAIN = 'https://nitter.poast.org';
-const url = `${DOMAIN}/ForoysktDaily`;
+// const url = `${DOMAIN}/ForoysktDaily`;
+const url = `${DOMAIN}/ForoysktDaily/with_replies`;
 
 log.info('Rendering', `<${url}> ...`);
 
@@ -17,23 +18,34 @@ log.info('Rendering', `<${url}> ...`);
  */
 async function getTweets(page) {
   log.info(`Querying for tweets on <${page.url()}> ...`);
-  const bodyHandle = await page.$('body');
-  const tweets = await page.evaluate((body, DOMAIN) => {
+  const tweets = (await page.evaluate(DOMAIN => {
     const nodes = Array.from(document.body.querySelectorAll('.timeline-item'));
 
     return nodes.map(node => {
+      // skip the "Load newest" button timeline item
+      if (node.classList.contains('show-more')) {
+        return null;
+      }
+
       const link = node.querySelector('.tweet-date a');
+
+      // something is wrong ...
+      if (!link) {
+        console.error('Missing date link', node.outerHTML);
+        return null;
+      }
+
+      const hasImage = !! node.querySelector('.still-image');
 
       return {
         link: DOMAIN + link.getAttribute('href'),
         date: link.getAttribute('title'), // e.g. Aug 13, 2024 · 10:28 PM UTC
         content: node.querySelector('.media-body').innerText,
-        image: DOMAIN + node.querySelector('.still-image img')?.getAttribute('src'),
-        image_large: DOMAIN + node.querySelector('.still-image')?.getAttribute('href'),
+        image: hasImage ? DOMAIN + node.querySelector('.still-image img')?.getAttribute('src') : undefined,
+        image_large: hasImage ? DOMAIN + node.querySelector('.still-image')?.getAttribute('href') : undefined,
       };
     });
-  }, bodyHandle, DOMAIN);
-  await bodyHandle.dispose();
+  }, DOMAIN)).filter(value => value);
 
   log.info(`Found ${tweets.length} tweet(s)`);
   return tweets;
@@ -44,6 +56,10 @@ async function getTweets(page) {
   // it will first load the page with HTTP 503 response, wait a bit and redirect to 200
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  await page.setViewport({
+    width: 640,
+    height: 800,
+  });
 
   page.on('requestfinished', (req) => {
     const url = req.url();
@@ -52,34 +68,55 @@ async function getTweets(page) {
     }
   });
 
-  page.on('console', msg => log.info('log: ', msg.text()));
+  page.on('console', msg => log.info('console.log', msg.text()));
 
   // https://pptr.dev/api/puppeteer.page.goto
-  const then = Date.now();
-
   // first request is 503, but it verifies the client
+  // subsequent request will give us 200
   await page.goto(url);
   await setTimeout(750);
 
-  // now we get HTTP 200
-  // TODO: load next subpages
+  // load all subpages
+  let all_tweets = [];
+
+  const then = Date.now();
   await page.goto(url, {waitUntil: 'networkidle0'});
   const took = Date.now() - then;
-
   log.info(`Page loaded`, `in ${took} ms`);
-  // await new Promise(resolve => setTimeout(resolve, 5000));
-  const tweets = await getTweets(page);
-  console.log(tweets);
 
-  // store the tweets
-  log.info(`Saving ${tweets.length} tweets ...`);
+  while(true) {
+    const tweets = await getTweets(page);
+    log.info('Page parsed', `found ${tweets.length} tweets`);
+
+    // append tweets from the current page to the list all of that profile
+    all_tweets = [...all_tweets, ...tweets];
+
+    // no more tweets
+    if (tweets.length < 1) {
+      break;
+    }
+
+    // find the next page
+    // https://pptr.dev/api/puppeteer.page._
+    // e.g. <div class="show-more"><a href="?cursor=DAABCgABGc8Zjhs__-gKAAIYHyMxmpbRVwgAAwAAAAIAAA">Load more</a></div>
+    // get the last node, on subpages we have buttons for newer and older posts
+    const node = (await page.$$('.show-more a')).at(-1);
+
+    if (!node) {
+      log.info('Next page?', 'No');
+      break;
+    }
+
+    await node.click();
+    await page.waitForNavigation();
+    await page.waitForNetworkIdle();
+  }
+
+  // store the all tweets
+  log.info(`Saving ${all_tweets.length} tweets ...`);
   const file = createWriteStream('tweets.json');
-  file.write(JSON.stringify(tweets, null, 2));
+  file.write(JSON.stringify(all_tweets, null, 2));
   file.end();
-
-  //log.info('Taking a screenshot ...');
-  //await page.setViewport({width: 1024, height: 8000, deviceScaleFactor: 2});
-  //await page.screenshot({path: 'nitter.webp', quality: 90});
 
   await browser.close();
   log.info('Done');
